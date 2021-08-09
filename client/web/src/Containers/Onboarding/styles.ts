55960a6caa915c7872a042d97684d0d71336b1e8
Johnny Rakoto <johnny.rakotoarison@gmail.com>
2021-08-09 18:08:46 +0300

    onboardin on progress...

--- client/web/src/Containers/Onboarding/styles.ts
+++ client/web/src/Containers/Onboarding/styles.ts
@@ -1,9 +1,15 @@
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(
   theme => ({
     root: {
-      background: "green"
+      
+    },
+    mainContainer: {
+
+    },
+    footerPage: {
+      
     },
   }),
   { name: "onboarding" }
