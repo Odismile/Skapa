6b2c1c6604724fb68efbff7dae9dbaca7c597b0a
Johnny Rakoto <johnny.rakotoarison@gmail.com>
2021-08-09 18:08:46 +0300

    onboardin on progress...

--- client/web/src/Containers/Onboarding/Onboarding.tsx
+++ client/web/src/Containers/Onboarding/Onboarding.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import { Box, Button } from "@material-ui/core";
-import {Link } from 'react-router-dom';
+import { Box, Button, Typography } from "@material-ui/core";
+import { Link } from 'react-router-dom';
 
 import classNames from "classnames";
 
 import useStyles from "./styles";
+
 
 const Onboarding = () => {
   const classes = useStyles();
   return (
     
     <Box className={classNames(classes.root)}>
-      <p>lorem</p>
+      <Box component="header">
+        <figure className="logo">
+          <img src="" alt="logo" />
+        </figure>
+      </Box>
+      <Box className={classNames(classes.mainContainer)}>
+        {/* slider content */}
+
+      </Box>
+      <Box component="footer" className={classes.footerPage} >
+      
+      </Box>
     </Box>
   );
 };
--- client/web/src/Theme/index.ts
+++ client/web/src/Theme/index.ts
@@ -1,12 +1,42 @@
-import { createTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core';
 
-const theme = createTheme({
+const theme = createMuiTheme ({
   typography: {
     fontFamily: 'Poppins',
     fontSize: 16,
+    body1: {
+      color: "#666",
+      fontSize: '1rem',
+      fontWeight: 400,
+      marginBottom: '0.5rem',
+    },
+    body2: {
+      color: "#666",
+      fontSize: '0.75rem',
+      fontWeight: 400,
+      marginBottom: '1rem',
+    },
   },
-  palette: {},
-  overrides: {},
+  palette: {
+    primary: {
+      main: '#9AC61E',
+    },
+    secondary: {
+      main: '#f5f5f5',
+    },
+    text: {
+      secondary: '#666',
+    },
+    success: {
+      main: '#9AC61E',
+    },
+    error: {
+      main: '#F2344B',
+    },
+    warning: {
+      main: '#F98436',
+    },
+  }
 });
 
 export default theme;
