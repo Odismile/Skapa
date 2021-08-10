c98ea303c9596afefd6d3bcf6b90402f9382ac48
Johnny Rakoto <johnny.rakotoarison@gmail.com>
2021-08-10 11:58:28 +0300

    onboarding

--- client/web/src/Containers/Onboarding/styles.ts
+++ client/web/src/Containers/Onboarding/styles.ts
@@ -5,8 +5,45 @@ export default makeStyles(
     root: {
       
     },
+    mainPage:  {
+      height: "100%",
+      position: "absolute",
+      width: "100%",
+      zIndex: 0,
+      "& .wrapPage": {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: "100%",
+        width: "100%",
+      },
+    },
+    main_header: {
+      "& .logo": {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        margin: 0,
+        "& img": {
+          maxWidth: "100%",
+          width: 105,
+        },
+      },
+    },
     mainContainer: {
 
+    },
+    sliderTuto: {
+     "& .tuto_slider" : {
+      "& .tuto_img" : {
+        margin: 0,
+        textAlign: "center",
+      },
+      "& .tuto_text": {
+        textAlign: "center",
+        
+      },
+     },
     },
     footerPage: {
       
