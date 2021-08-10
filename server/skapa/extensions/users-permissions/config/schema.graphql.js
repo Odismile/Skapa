fa6d8bd022532a1a66e3f3d6bf8110531b33abd8
hasina <hasina@hairun-technology.com>
2021-08-10 15:13:05 +0300

    Custom user permissions

---
+++ server/skapa/extensions/users-permissions/config/schema.graphql.js
@@ -0,0 +1,69 @@
+const _ = require("lodash");
+
+/**
+ * Throws an ApolloError if context body contains a bad request
+ * @param contextBody - body of the context object given to the resolver
+ * @throws ApolloError if the body is a bad request
+ */
+function checkBadRequest(contextBody) {
+  if (_.get(contextBody, "statusCode", 200) !== 200) {
+    const message = _.get(contextBody, "error", "Bad Request");
+    const exception = new Error(message);
+    exception.code = _.get(contextBody, "statusCode", 400);
+    exception.data = contextBody;
+    throw exception;
+  }
+}
+
+module.exports = {
+  type: {
+    UsersPermissionsPermission: false, // Make this type NOT queriable.
+  },
+  definition: /* GraphQL */ `
+    type UsersPermissionsCustomizeMe {
+      id: ID!
+      username: String!
+      email: String!
+      confirmed: Boolean
+      blocked: Boolean
+      lastname: String
+      surname: String
+      role: UsersPermissionsMeRole
+    }
+
+    type UsersPermissionsLoginCustomizePayload {
+      jwt: String
+      user: UsersPermissionsCustomizeMe!
+    }
+  `,
+  mutation: `
+    loginCustom(input: UsersPermissionsLoginInput!): UsersPermissionsLoginCustomizePayload!
+  `,
+  resolver: {
+    Mutation: {
+      loginCustom: {
+        resolverOf: "plugins::users-permissions.auth.callback",
+        resolver: async (obj, options, { context }) => {
+          context.params = {
+            ...context.params,
+            provider: options.input.provider,
+          };
+          context.request.body = _.toPlainObject(options.input);
+
+          await strapi.plugins["users-permissions"].controllers.auth.callback(
+            context
+          );
+          let output = context.body.toJSON
+            ? context.body.toJSON()
+            : context.body;
+
+          checkBadRequest(output);
+          return {
+            user: output.user || output,
+            jwt: output.jwt,
+          };
+        },
+      },
+    },
+  },
+};
