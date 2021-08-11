9ee407e5ac1a67c9b21c6e51f22a96f17f4440c7
Johnny Rakoto <johnny.rakotoarison@gmail.com>
2021-08-11 17:49:53 +0300

    description component update

--- client/web/src/Containers/Project/CreateProject/Description/Description.tsx
+++ client/web/src/Containers/Project/CreateProject/Description/Description.tsx
@@ -1,10 +1,11 @@
 import React, { useRef, useState } from 'react';
-import { Box, Button, Container, Typography, IconButton } from "@material-ui/core";
+import { Box, Button, Container, Typography, IconButton, TextField } from "@material-ui/core";
 import { Link } from 'react-router-dom';
 import classNames from "classnames";
 
-import TextField from '../../../../Components/TextField/TextField';
+import TextFieldComponent from '../../../../Components/TextField/TextField';
 import IconPhoto from '../../../../Components/Icons/Photo/Photo';
+import Calendar from '../../../../Components/Icons/Calendar/Calendar';
 import EditIcon from '@material-ui/icons/Edit';
 
 import useStyles from "./styles";
@@ -32,24 +33,49 @@ const Description = () => {
       
       {/* Forms content */}
       <Box className="form_bloc">
-        <Box className="item_bloc">
-          <Box className="title_bloc" component="header">
-            <Typography variant="h2">Lorem Ipsum Sit Amet  </Typography>
-            <IconButton aria-label="edit" className="btn_edit">
-              <EditIcon/>
-            </IconButton>
-            <Typography className="subTitle" variant="h3">Basic informations</Typography>
+        <form className="formDescription">
+          <Box className="item_bloc">
+            <Box className="title_bloc" component="header">
+              <Typography variant="h2">Lorem Ipsum Sit Amet  </Typography>
+              <IconButton aria-label="edit" className="btn_edit">
+                <EditIcon/>
+              </IconButton>
+              <Typography className="subTitle" variant="h3">Basic informations</Typography>
+            </Box>
+            <Box className="content_bloc" component="section">
+              <Box className="field_item typeProject_item">
+                <TextFieldComponent label="Type project" id="TypeProject" placeholder="Type project" type="text" />
+              </Box>
+              <Box className="field_item">
+                <TextFieldComponent label="Ville" id="VilleProject" placeholder="Ville " type="text" />
+              </Box>
+              <Box className="grid_field">
+                <Box className="field_item">
+                <TextField
+                  id="date"
+                  label="Starts"
+                  type="date"
+                  variant="outlined"
+                  defaultValue="12/05/21"
+                  className="dateField"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+                  {/* <IconButton aria-label="date" className="btn_append btn_datePicker">
+                    <Calendar/>
+                  </IconButton> */}
+                </Box>
+                {/* <Box className="field_item">
+                  <TextFieldComponent label="Ends" id="EndsProject" placeholder="dd/mm/yy" type="text" />
+                </Box> */}
+              </Box>
+            </Box>
           </Box>
-          <Box className="content_bloc" component="section">
-            <TextField label="Type project" id="TypeProject" placeholder="Type project" type="text" />
-            <TextField label="Ville" id="VilleProject" placeholder="Ville " type="text" />
+          <Box className="item_bloc">
 
           </Box>
-        </Box>
-
-        <Box className="title_bloc">
-
-        </Box>
+        </form>
       </Box>
     </Box>
   );
