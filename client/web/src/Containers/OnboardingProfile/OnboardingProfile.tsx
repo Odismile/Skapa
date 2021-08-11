0ecad4db1eeb21bcb36411bdda4e76da73cb5b0b
Fanasina Ny <fanasina@hairun-technology.com>
2021-08-11 10:07:03 +0300

    wrap component onboarding

--- client/web/src/Containers/OnboardingProfile/OnboardingProfile.tsx
+++ client/web/src/Containers/OnboardingProfile/OnboardingProfile.tsx
@@ -15,8 +15,9 @@ import useStyles from './styles';
 import logoTalent from '../../Assets/images/logo-talent.svg';
 import LanguagesChoice from '../../Components/LanguagesChoice/LanguagesChoice';
 import { useHistory } from 'react-router-dom';
-import classNames from "classnames";
-import mainLogo from "../../Assets/images/logo.svg"
+import classNames from 'classnames';
+import mainLogo from '../../Assets/images/logo.svg';
+import WrapOnBoarding from '../../Components/WrapOnBoarding/WrapOnBoarding';
 
 const OnboardingProfile = () => {
   const classes = useStyles();
@@ -33,41 +34,36 @@ const OnboardingProfile = () => {
 
   return (
     <>
-      <Box className={classes.mainPage}>
-        <Box className="wrapPage">
-          <Box component="header" className={classNames(classes.main_header)}>
-            <figure className="logo">
-              <img src={mainLogo} alt="logo" />
-            </figure>
-          </Box>
-          <Box className={classNames(classes.mainContainer)}>
-          <TextFieldComponent label="Your position" id="position" placeholder="UX Designer" type="text" />
-          <FormControl component="fieldset" className={classes.radio}>
-            <FormLabel component="legend">Job Seniority (years)</FormLabel>
-            <RadioGroup row aria-label="years" name="years" value={value} onChange={handleChange} className='custom_radio'>
-              <FormControlLabel value="0-3" control={<Radio />} label="0-3 yrs" />
-              <FormControlLabel value="3-8" control={<Radio />} label="3-8 yrs" />
-              <FormControlLabel value=">8" control={<Radio />} label="> 8 yrs" />
-            </RadioGroup>
-          </FormControl>
-          <TextFieldComponent label="Name of your organisation" id="organisation" placeholder="GRT Gaz" type="text" />
-          <FormControl component="fieldset" className={classes.languages}>
-            <FormLabel  component="legend">Languages level</FormLabel>
-            <LanguagesChoice title="French"/>
-            <LanguagesChoice title="English"/>
-            <LanguagesChoice title="Spanish"/>
-          </FormControl>
-          <Box className={classes.btnNext}>
-            <Button variant="contained" onClick={handleClick}>Next</Button>
-          </Box>
-          </Box>
-          <Box component="footer" className={classes.footerPage}>
-            <Typography className="link-footer">
-              <Link href="#">Skip this step</Link>
-            </Typography>
-          </Box>
+      <WrapOnBoarding>
+        <TextFieldComponent label="Your position" id="position" placeholder="UX Designer" type="text" />
+        <FormControl component="fieldset" className={classes.radio}>
+          <FormLabel component="legend">Job Seniority (years)</FormLabel>
+          <RadioGroup
+            row
+            aria-label="years"
+            name="years"
+            value={value}
+            onChange={handleChange}
+            className="custom_radio"
+          >
+            <FormControlLabel value="0-3" control={<Radio />} label="0-3 yrs" />
+            <FormControlLabel value="3-8" control={<Radio />} label="3-8 yrs" />
+            <FormControlLabel value=">8" control={<Radio />} label="> 8 yrs" />
+          </RadioGroup>
+        </FormControl>
+        <TextFieldComponent label="Name of your organisation" id="organisation" placeholder="GRT Gaz" type="text" />
+        <FormControl component="fieldset" className={classes.languages}>
+          <FormLabel component="legend">Languages level</FormLabel>
+          <LanguagesChoice title="French" />
+          <LanguagesChoice title="English" />
+          <LanguagesChoice title="Spanish" />
+        </FormControl>
+        <Box className={classes.btnNext}>
+          <Button variant="contained" onClick={handleClick}>
+            Next
+          </Button>
         </Box>
-      </Box>
+      </WrapOnBoarding>
     </>
   );
 };
--- client/web/src/Containers/OnboardingProfileTwo/OnboardingProfileTwo.tsx
+++ client/web/src/Containers/OnboardingProfileTwo/OnboardingProfileTwo.tsx
@@ -5,6 +5,7 @@ import iconDownload from '../../Assets/images/IconDownload.svg';
 import { useHistory } from 'react-router';
 import classNames from 'classnames';
 import mainLogo from '../../Assets/images/logo.svg';
+import WrapOnBoarding from '../../Components/WrapOnBoarding/WrapOnBoarding';
 
 const OnboardingProfileTwo = () => {
   const classes = useStyles();
@@ -16,56 +17,42 @@ const OnboardingProfileTwo = () => {
 
   return (
     <>
-      <Box className={classes.mainPage}>
-        <Box className="wrapPage">
-          <Box component="header" className={classNames(classes.main_header)}>
-            <figure className="logo">
-              <img src={mainLogo} alt="logo" />
-            </figure>
-          </Box>
-          <Box className={classNames(classes.mainContainer)}>
-            <FormControl component="fieldset" className={classes.form}>
-              <FormLabel component="legend" className="title">
-                Upload a picture of you
-              </FormLabel>
-              <input accept="image/*" className={classes.input} id="contained-button-file" multiple type="file" />
-              <label htmlFor="contained-button-file">
-                <Button variant="contained" component="span">
-                  +
-                </Button>
-              </label>
-            </FormControl>
-            <FormControl component="fieldset" className={classes.inputVideo}>
-              <FormLabel component="legend" className="title">
-                Upload a video pitch
-              </FormLabel>
-              <input accept="video/*" className={classes.input} id="contained-button-file" multiple type="file" />
-              <label htmlFor="contained-button-file">
-                <Box className="video">
-                  <Box component="span">Import a video from your computer (mp4)</Box>
-                  <img src={iconDownload} alt="iconDownload" />
-                </Box>
-              </label>
-            </FormControl>
-            <FormControl component="fieldset" className={classes.bio}>
-              <FormLabel component="legend" className="title">
-                Bio
-              </FormLabel>
-              <textarea placeholder="tell us more about you !"></textarea>
-            </FormControl>
-            <Box className={classes.btnNext}>
-              <Button variant="contained" onClick={handleClick}>
-                Next
-              </Button>
+      <WrapOnBoarding>
+        <FormControl component="fieldset" className={classes.form}>
+          <FormLabel component="legend" className="title">
+            Upload a picture of you
+          </FormLabel>
+          <input accept="image/*" className={classes.input} id="contained-button-file" multiple type="file" />
+          <label htmlFor="contained-button-file">
+            <Button variant="contained" component="span">
+              +
+            </Button>
+          </label>
+        </FormControl>
+        <FormControl component="fieldset" className={classes.inputVideo}>
+          <FormLabel component="legend" className="title">
+            Upload a video pitch
+          </FormLabel>
+          <input accept="video/*" className={classes.input} id="contained-button-file" multiple type="file" />
+          <label htmlFor="contained-button-file">
+            <Box className="video">
+              <Box component="span">Import a video from your computer (mp4)</Box>
+              <img src={iconDownload} alt="iconDownload" />
             </Box>
-          </Box>
-          <Box component="footer" className={classes.footerPage}>
-            <Typography className="link-footer">
-              <Link href="#">Skip this step</Link>
-            </Typography>
-          </Box>
+          </label>
+        </FormControl>
+        <FormControl component="fieldset" className={classes.bio}>
+          <FormLabel component="legend" className="title">
+            Bio
+          </FormLabel>
+          <textarea placeholder="tell us more about you !"></textarea>
+        </FormControl>
+        <Box className={classes.btnNext}>
+          <Button variant="contained" onClick={handleClick}>
+            Next
+          </Button>
         </Box>
-      </Box>
+      </WrapOnBoarding>
     </>
   );
 };
