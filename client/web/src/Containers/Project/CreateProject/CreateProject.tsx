8cb0bf2fd10b8fb9893d8ebd70647209e7af6510
Johnny Rakoto <johnny.rakotoarison@gmail.com>
2021-08-11 10:35:05 +0300

    create project

---
+++ client/web/src/Containers/Project/CreateProject/CreateProject.tsx
@@ -0,0 +1,30 @@
+import React, { useRef, useState } from 'react';
+import { Box, Button, Container, Typography } from "@material-ui/core";
+import { Link } from 'react-router-dom';
+import classNames from "classnames";
+
+import mainLogo from "../../Assets/images/logo.svg"
+import useStyles from "./styles";
+import PrimaryHeader from '../../../Components/Header';
+
+// install Swiper modules
+
+
+const CreateProject = () => {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.mainPage}>
+      <Box className="wrapPage">
+        {/* header component */}
+        <PrimaryHeader />
+
+        <Box className={classes.mainContainer}>
+          <p>Create Project Page here !</p>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default CreateProject;
