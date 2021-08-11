bf124ca9ed9bf73ef4be1e806b806d76e7cd0bf1
Johnny Rakoto <johnny.rakotoarison@gmail.com>
2021-08-11 17:50:09 +0300

    icon

---
+++ client/web/src/Components/Icons/Calendar/Calendar.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+import { SvgIcon, SvgIconProps } from '@material-ui/core';
+
+export default (props: SvgIconProps) => {
+  return (
+    <SvgIcon {...props} viewBox="0 0 13 14">
+      <g>
+        <path d="M0.999 0L11.601 0Q11.6994 0 11.7959 0.0191955Q11.8924 0.038391 11.9833 0.0760443Q12.0742 0.113698 12.156 0.168362Q12.2378 0.223026 12.3074 0.2926Q12.377 0.362175 12.4316 0.443985Q12.4863 0.525796 12.524 0.616699Q12.5616 0.707602 12.5808 0.804105Q12.6 0.900607 12.6 0.999L12.6 11.601Q12.6 11.6994 12.5808 11.7959Q12.5616 11.8924 12.524 11.9833Q12.4863 12.0742 12.4316 12.156Q12.377 12.2378 12.3074 12.3074Q12.2378 12.377 12.156 12.4316Q12.0742 12.4863 11.9833 12.524Q11.8924 12.5616 11.7959 12.5808Q11.6994 12.6 11.601 12.6L0.999 12.6Q0.900607 12.6 0.804105 12.5808Q0.707602 12.5616 0.616699 12.524Q0.525796 12.4863 0.443985 12.4316Q0.362175 12.377 0.2926 12.3074Q0.223026 12.2378 0.168362 12.156Q0.113698 12.0742 0.0760443 11.9833Q0.038391 11.8924 0.0191955 11.7959Q0 11.6994 0 11.601L0 0.999Q0 0.900607 0.0191955 0.804105Q0.038391 0.707602 0.0760443 0.616699Q0.113698 0.525796 0.168362 0.443985Q0.223026 0.362175 0.2926 0.2926Q0.362175 0.223026 0.443985 0.168362Q0.525796 0.113698 0.616699 0.0760443Q0.707602 0.038391 0.804105 0.0191955Q0.900607 0 0.999 0Z" transform="translate(0 1.4)" id="Rectangle" fill="none" fill-rule="evenodd" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M0.35 0L0.35 2.8" transform="translate(8.75 0)" id="Path" fill="none" fill-rule="evenodd" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M0.35 0L0.35 2.8" transform="translate(3.15 0)" id="Path" fill="none" fill-rule="evenodd" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M0 0.35L12.6 0.35" transform="translate(0 5.25)" id="Path" fill="none" fill-rule="evenodd" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" />
+      </g>
+    </SvgIcon>
+  );
+};
