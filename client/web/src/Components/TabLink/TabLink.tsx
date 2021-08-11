acbdc929868bc91d2c3ecba05c53cecee715e570
Johnny Rakoto <johnny.rakotoarison@gmail.com>
2021-08-11 16:21:50 +0300

    styling tablink

--- client/web/src/Components/TabLink/TabLink.tsx
+++ client/web/src/Components/TabLink/TabLink.tsx
@@ -1,31 +1,37 @@
-import React from 'react';
-import { Box, Typography } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import React, { FC } from 'react';
+import { Box, Typography} from '@material-ui/core';
+import {Switch, Route, Link } from "react-router-dom";
 import classNames from "classnames";
+
+import Description from '../../Containers/Project/CreateProject/Description/Description';
+import Team from '../../Containers/Project/CreateProject/Team/Team';
+import Places from '../../Containers/Project/CreateProject/Places/Places';
+import Review from '../../Containers/Project/CreateProject/Review/Review';
+
 import useStyles from './style';
 
-// icons
-import Burger from '../Icons/Burger/Burger';
-import Award from '../Icons/Award/Award';
-import HeartLine from '../Icons/HeartLine/HeartLine';
-import Photo from '../Icons/Photo/Photo';
 
-
-const PrimaryHeader = () => {
+const TabLink = () => {
   const classes = useStyles();
   
   return (
     <Box className={classes.tabList}>
-      <Box className="header_content">
-        {/* burger menu mobile */}
-        <p>TabList here !</p>
-
-        {/* logo mobile */}
-
-        {/* list of notification */}
+      <Box className="tab_list">
+        <Link to="description" className="tab_link active">Description</Link>
+        <Link to="team" className="tab_link">Team</Link>
+        <Link to="places" className="tab_link">Places</Link>
+        <Link to="review" className="tab_link">Review</Link>
+      </Box>
+      <Box className="tab_content">
+        <Switch>
+          <Route path="description" component={Description} />
+          <Route path="team" component={Team} />
+          <Route path="places" component={Places} />
+          <Route path="review" component={Review} />
+        </Switch>
       </Box>
     </Box>
   );
 };
 
-export default PrimaryHeader;
+export default TabLink;
