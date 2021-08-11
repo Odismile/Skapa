474e03753e4b27e0d9f505f4155d67d71032684e
Odilon <odilon@hairun-technology.com>
2021-08-11 09:56:48 +0200

    restructureation page onboarding profile 6

--- client/web/src/Containers/OnboardingProfileSix/OnboardingProfileSix.tsx
+++ client/web/src/Containers/OnboardingProfileSix/OnboardingProfileSix.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react';
 import { Box, Button, Container, Typography } from "@material-ui/core";
 import { Link } from 'react-router-dom';
 import classNames from "classnames";
+import WrapOnBoarding from '../../Components/WrapOnBoarding/WrapOnBoarding';
 
 import mainLogo from "../../Assets/images/logo.svg"
 import useStyles from "./style";
@@ -10,28 +11,16 @@ const OnboardingProfileSix = () => {
   const classes = useStyles();
 
   return (
-    <Box className={classNames(classes.mainPage)}>
-      <Box className="wrapPage">
-        <Box component="header" className={classNames(classes.main_header)}>
-          <figure className="logo">
-            <img src={mainLogo} alt="logo" />
-          </figure>
+    <>
+      <WrapOnBoarding>
+        <Box className={classes.onBoard}>
+          <p>What are your goals using Talent Team ?</p>
         </Box>
-        <Box className={classNames(classes.mainContainer)}>
-          <Box className="onboard-profile-content">
-            <p>What are your goals using Talent Team ?</p>
-            <Box className={classes.btnNext}>
-              <Button variant="contained">Next</Button>
-            </Box>
-          </Box>
+        <Box className={classes.btnNext}>
+          <Button variant="contained">Next</Button>
         </Box>
-        <Box component="footer" className={classes.footerPage} >
-          <Typography className="link-footer">
-            <Link to="/">Skip this step</Link>
-          </Typography>
-        </Box>
-      </Box>
-    </Box>
+      </WrapOnBoarding>
+    </>
   );
 };
 
