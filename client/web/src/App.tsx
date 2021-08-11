c3a4e85a3ff0e3be3b26ccdc07551a3f63f977e3
Johnny Rakoto <johnny.rakotoarison@gmail.com>
2021-08-11 11:05:10 +0300

    mount theme in app

--- client/web/src/App.tsx
+++ client/web/src/App.tsx
@@ -1,5 +1,8 @@
 import React, { FC } from 'react';
 import { Redirect, Route, Switch, withRouter, RouteComponentProps } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/styles';
+import { CssBaseline } from '@material-ui/core';
+import Theme from './Theme/index';
 
 import { ROUTES, UNAUTHORIZED } from './Routes';
 import { RouteInterface } from './Interfaces/Route';
@@ -7,22 +10,27 @@ import PrivateRoute from './Components/PrivateRoute';
 import NotFound from './Containers/Errors/NotFound';
 import Unauthorized from './Containers/Errors/Unauthorized';
 
+
+
 const App: FC<RouteComponentProps> = (props) => {
   const { history } = props;
 
   return (
     <>
-      <Switch>
-        <Route exact path={UNAUTHORIZED} component={Unauthorized} />
-        {ROUTES.map((route: RouteInterface) =>
-          route.private ? (
-            <PrivateRoute exact key={route.key} path={route.route} component={route.component} role={route.roles} />
-          ) : (
-            <Route exact key={route.key} path={route.route} component={route.component} />
-          ),
-        )}
-        <Route component={NotFound} />
-      </Switch>
+      <ThemeProvider theme={Theme}>
+        <CssBaseline />
+        <Switch>
+          <Route exact path={UNAUTHORIZED} component={Unauthorized} />
+          {ROUTES.map((route: RouteInterface) =>
+            route.private ? (
+              <PrivateRoute exact key={route.key} path={route.route} component={route.component} role={route.roles} />
+            ) : (
+              <Route exact key={route.key} path={route.route} component={route.component} />
+            ),
+          )}
+          <Route component={NotFound} />
+        </Switch>
+      </ThemeProvider>
     </>
   );
 };
