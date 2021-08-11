a10d48464211c53c54ee10809154f474399db69f
Johnny Rakoto <johnny.rakotoarison@gmail.com>
2021-08-11 16:21:50 +0300

    styling tablink

--- client/web/src/Components/TabLink/style.ts
+++ client/web/src/Components/TabLink/style.ts
@@ -1,10 +1,50 @@
 import { makeStyles } from '@material-ui/core/styles';
 
+
+
 export default makeStyles (
   theme => ({
     tabList: {
-      
-    }
+      height: 15,
+      marginBottom: 20,
+      marginTop: 20,
+      "& .tab_list": {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        "& .tab_link": {
+          color: '#000',
+          display: "inline-flex",
+          fontSize: 14,
+          fontWeight: 400,
+          padding: "0 8px",
+          position: 'relative',
+          textDecoration: "none",
+          "&:before": {
+            background: theme.palette.primary.main,
+            content: "''",
+            height: 15,
+            position: 'absolute',
+            right: 0,
+            top: "50%",
+            transform: "translateY(-50%)",
+            width: 1,
+          },
+          "&.active": {
+            color: theme.palette.primary.main,
+            fontWeight: 700,
+          },
+          "&:last-child": {
+            "&:before": {
+              display: 'none',
+            }
+          },
+        },
+      },
+      "& .tab_content": {
+
+      },
+    },
   }),
   { name: "TabLink" }
 );
