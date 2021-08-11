cb82140fad12a413b15673d975a0fba37dc8c787
Keke <keke@hairun-technology.com>
2021-08-11 11:48:03 +0300

    web: generate query items

---
+++ client/web/src/GraphQL/items/types/Item.ts
@@ -0,0 +1,24 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+import { PublicationState } from "./../../../types/graphql-global-types";
+
+// ====================================================
+// GraphQL query operation: Item
+// ====================================================
+
+export interface Item_item {
+  __typename: "Items";
+  id: string;
+}
+
+export interface Item {
+  item: Item_item | null;
+}
+
+export interface ItemVariables {
+  id: string;
+  publicationState?: PublicationState | null;
+}
