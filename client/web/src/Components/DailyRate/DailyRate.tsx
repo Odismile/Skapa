5e822df6666c93ea2f8ef07c1a983d6ddc53e60c
Fanasina Ny <fanasina@hairun-technology.com>
2021-08-13 16:23:58 +0300

    component daily rate

---
+++ client/web/src/Components/DailyRate/DailyRate.tsx
@@ -0,0 +1,21 @@
+import { Box, Slider, Typography } from '@material-ui/core';
+import useStyles from './styles';
+
+const DailyRate = () => {
+  const classes = useStyles();
+  return (
+    <Box className={classes.root}>
+      <Typography className="money">100£ - 880£</Typography>
+      <Typography className="text">The average rate is 675£</Typography>
+      <Slider
+        orientation="horizontal"
+        defaultValue={[20, 37]}
+        aria-labelledby="vertical-slider"
+        valueLabelDisplay="on"
+        classes={{ valueLabel:classes.label }}
+      />
+    </Box>
+  );
+};
+
+export default DailyRate;
