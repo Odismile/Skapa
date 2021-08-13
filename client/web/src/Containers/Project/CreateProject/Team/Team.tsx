ad03a203e30250b22c0737eb4c14bcae52b626c3
Odilon <odilon@hairun-technology.com>
2021-08-13 19:58:09 +0200

    integration team create project

--- client/web/src/Containers/Project/CreateProject/Team/Team.tsx
+++ client/web/src/Containers/Project/CreateProject/Team/Team.tsx
@@ -1,15 +1,66 @@
 import React  from 'react';
-import { Box } from "@material-ui/core";
+import { Box, Link, Typography } from "@material-ui/core";
 import useStyles from "./styles";
-import SearchFilter from '../../../../Components/SearchFilter/SearchFilter';
+import CardTalents from '../../../../Components/CardTalents/CardTalents';
 
 
 const Team = () => {
   const classes = useStyles();
-
+  
   return (
     <Box className={classes.teamBloc}>
-      <SearchFilter />
+      <Box className="team-item">
+        <Box className="team-item-top">
+          <Typography component="h2">
+            Selected profiles
+          </Typography>
+          <Typography component="span" className="price">
+            14 000 $
+          </Typography>
+        </Box>
+        <Box className="cardtalent-item">
+          <CardTalents />
+        </Box>
+        <Box className="cardtalent-item">
+          <CardTalents />
+        </Box>
+        <Box className='link'>
+          <Link href='#'>Remove selected profile</Link>
+        </Box>
+        
+      </Box>
+      <Box className="team-item">
+        <Box className="team-item-top">
+          <Typography component="h2">
+            Add from recommandations
+          </Typography>
+        </Box>
+        <Box className="cardtalent-item">
+          <CardTalents />
+        </Box>
+        <Box className="cardtalent-item">
+          <CardTalents />
+        </Box>
+        <Box className='link'>
+          <Link href='#'>Add selected profile</Link>
+        </Box>
+      </Box>
+      <Box className="team-item">
+        <Box className="team-item-top">
+          <Typography component="h2">
+            Add from favorites
+          </Typography>
+        </Box>
+        <Box className="cardtalent-item">
+          <CardTalents />
+        </Box>
+        <Box className="cardtalent-item">
+          <CardTalents />
+        </Box>
+        <Box className='link'>
+          <Link href='#'>Add selected profile</Link>
+        </Box>
+      </Box>
     </Box>
   );
 };
--- client/web/src/Containers/Project/CreateProject/Team/styles.ts
+++ client/web/src/Containers/Project/CreateProject/Team/styles.ts
@@ -3,6 +3,37 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles(
   theme => ({
     teamBloc: {
+      "& .team-item": {
+        marginBottom: 20,
+        "& .team-item-top": {
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        },
+        "& .price": {
+          padding: "10px 15px",
+          borderRadius: 25,
+          background: "#dffff2",
+          fontWeight: "600",
+          color: "#5bd3a0",
+        },
+        "& h2": {
+          fontSize: 14,
+          fontWeight: "700",
+          color: "#000",
+          marginBottom: 13,
+        },
+        "& .cardtalent-item": {
+          marginBottom: 10,
+        },
+        "& .link": {
+          display: "flex",
+          justifyContent: "flex-end",
+          "& a": {
+            borderBottom: "1px solid #8870FF",
+          }
+        }
+      }
     },
   }),
   { name: "Team" }
