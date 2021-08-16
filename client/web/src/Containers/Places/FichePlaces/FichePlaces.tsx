a678ea58e122829b58e3060f1775f4ddd56f5c57
Odilon <odilon@hairun-technology.com>
2021-08-16 09:39:14 +0200

    ajout contenu detail places

--- client/web/src/Containers/Places/FichePlaces/FichePlaces.tsx
+++ client/web/src/Containers/Places/FichePlaces/FichePlaces.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Divider, Link, Typography } from '@material-ui/core';
+import { Box, Button, Divider, Link, TextField, Typography } from '@material-ui/core';
 import FormerProject from '../../../Components/FormerProjects/FormerProjects';
 import Pitch from '../../../Components/Pitch/Pitch';
 import Place from '../../../Components/Place/Place';
@@ -7,13 +7,20 @@ import SimpleCardTalents from '../../../Components/SimpleCardTalents/SimpleCardT
 import useStyles from './styles';
 import image_place1 from '../../../Assets/images/place-1.png';
 import Wifi from '../../../Components/Icons/Wifi';
+import Board from '../../../Components/Icons/Board';
+import Clipboard from '../../../Components/Icons/Clipboard';
+import Coffee from '../../../Components/Icons/Coffee';
+import Cast from '../../../Components/Icons/Cast';
+import Computer from '../../../Components/Icons/Computer';
 
 
 const FichePlaces = () => {
   const classes = useStyles();
   return (
     <Box className={classes.root}>
-      <Place />
+      <Box className="places">
+        <Place />
+      </Box>
       <Box className='presentation'>
         <Typography variant="h6" className="title">
           Présentation
@@ -40,42 +47,79 @@ const FichePlaces = () => {
         </Box>
       </Box>
       <Box className="equipement">
+        <Typography variant="h6" className="title">
+          Equipements
+        </Typography>
         <Box className="equipement-item">
           Wifi <Wifi />
         </Box>
         <Box className="equipement-item">
-          Wifi <Wifi />
+          White board <Board />
         </Box>
         <Box className="equipement-item">
-          Wifi <Wifi />
+          Paper board <Clipboard />
         </Box>
         <Box className="equipement-item">
-          Wifi <Wifi />
+          Coffee machine and snacks <Coffee />
         </Box>
         <Box className="equipement-item">
-          Wifi <Wifi />
+          TV broadcast <Cast />
         </Box>
         <Box className="equipement-item">
-          Wifi <Wifi />
+          Computer <Computer />
         </Box>
+      </Box>
+      <Box className="disponibilite">
+        <Typography variant="h6" className="title">
+          Disponibility
+        </Typography>
+        <Box className="grid_field">
+                <Box className="field_item field_date">
+                  <TextField
+                    id="dateStarts"
+                    label="Starts"
+                    type="date"
+                    defaultValue="12/05/21"
+                    className="input_date"
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                </Box>
+                <Box className="field_item field_date">
+                  <TextField
+                    id="dateEnd"
+                    label="Ends"
+                    type="date"
+                    defaultValue="12/05/21"
+                    className="input_date"
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                </Box>
+              </Box>
+      </Box>
+      <Box className="contact">
+        <Typography variant="h6" className="title">
+        Contact
+        </Typography>
+        <p>
+          <span>Phone :</span> +33 1 56 45 78 96
+        </p>
+        <p>
+          <span>Email : </span> wework.lafayette@wework.fr
+        </p>
+      </Box>
+      <Box className="boxBtn">
+        <Button className="btnAdd">Book this place for a project</Button>
       </Box>
       <Box>
         <Typography variant="h6" className="title">
-          Former projects
+          Similar places
         </Typography>
         <FormerProject />
       </Box>
-      <Box>
-        <Pitch />
-      </Box>
-      <Box className="boxBtn">
-        <Button className="btnAdd">Add to Project</Button>
-      </Box>
-      <Divider />
-      <Typography className="textSuggest">
-        You like Emma ? <br></br>You will like :
-      </Typography>
-      <SimpleCardTalents />
     </Box>
   );
 };
