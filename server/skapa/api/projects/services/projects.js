8f143ce935a0dd1e46aaf65733ed69e10a391aab
liantsoa <sarobidyrova@hairun-technology.com>
2021-08-16 18:00:38 +0100

    create customer query strapi

--- server/skapa/api/projects/services/projects.js
+++ server/skapa/api/projects/services/projects.js
@@ -1,8 +1,18 @@
 'use strict';
-
+import {generateReadSignedUrl} from "../../../Utils/firebaseCloudStorage"
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+    async find(params, populate) {
+        console.log("tonga ato tsika");
+        let az = await generateReadSignedUrl("/project/profile/76/kymco.jpg");
+        console.log(az);
+        let picture = "";
+        const result = strapi.query('projects').find(params, populate);
+        result.then((res)=>{console.log("result : ", res); picture = res.picture});
+        console.log("result : ", result);
+    }
+};
