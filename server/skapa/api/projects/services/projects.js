a0bff2ab3230513191b8f23d3da8f0dc4204a3c3
hasina <hasina@hairun-technology.com>
2021-08-16 21:52:10 +0300

    Custom create project

--- server/skapa/api/projects/services/projects.js
+++ server/skapa/api/projects/services/projects.js
@@ -1,8 +1,34 @@
-'use strict';
+"use strict";
+const { isDraft } = require("strapi-utils").contentTypes;
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  async create(data, { files } = {}) {
+    const project_skills = data.project_skills;
+    const items = await strapi
+      .query("items")
+      .find({ label_in: project_skills, type: "SKILLS" });
+    const selectedIds = items.map(({ id }) => id);
+
+    delete data.project_skills;
+
+    const entry = await strapi.query("projects").create(data);
+
+    const createPS = async function (projecId, skillId) {
+      const prSkill = await strapi.query("project-skills").create({
+        project: projecId,
+        item: skillId,
+      });
+    };
+
+    selectedIds.forEach((elt) => {
+      createPS(entry.id, elt);
+    });
+
+    return entry;
+  },
+};
