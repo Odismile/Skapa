712d3aeca3eb3f35af915d030961d5ab6841c84e
Odilon <odilon@hairun-technology.com>
2021-08-16 09:38:38 +0200

    ajout icons compontents board, clipboard, coffee

---
+++ client/web/src/Components/Icons/Coffee/Coffee.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+import { SvgIcon, SvgIconProps } from '@material-ui/core';
+
+export default (props: SvgIconProps) => {
+  return (
+    <SvgIcon {...props} viewBox="0 0 15 12">
+      <g>
+      <path d="M0 0L0.8 0C2.56731 0 4 1.34315 4 3C4 4.65685 2.56731 6 0.8 6L0 6" transform="translate(11 5)" id="Path" fill="none" fill-rule="evenodd" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M0 0L11 0L11 6.23077C11 7.76017 9.76878 9 8.25 9L2.75 9C1.23122 9 3.05311e-16 7.76017 0 6.23077L0 0Z" transform="translate(0 5)" id="Path" fill="none" fill-rule="evenodd" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M0.5 0L0.5 2" transform="translate(2 0)" id="Path" fill="none" fill-rule="evenodd" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M0.5 0L0.5 2" transform="translate(5 0)" id="Path" fill="none" fill-rule="evenodd" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M0.5 0L0.5 2" transform="translate(8 0)" id="Path" fill="none" fill-rule="evenodd" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" />
+      </g>
+    </SvgIcon>
+  );
+};
