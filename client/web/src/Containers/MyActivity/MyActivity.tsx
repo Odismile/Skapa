3ec459b488b0a681b05d2dd8bd478a516773a467
Fanasina Ny <fanasina@hairun-technology.com>
2021-08-18 16:11:24 +0300

    card contributor

--- client/web/src/Containers/MyActivity/MyActivity.tsx
+++ client/web/src/Containers/MyActivity/MyActivity.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import { Box } from "@material-ui/core";
-import PrimaryHeader from "../../Components/Header";
-import useStyles from "./styles";
+import { Box } from '@material-ui/core';
+import PrimaryHeader from '../../Components/Header';
+import useStyles from './styles';
 import UserActivityList from '../../Components/MyActivity/UserActivityList/UserActivityList';
 import UserContributorList from '../../Components/MyActivity/UserContributorList/UserContributorList';
 import UserTalentList from '../../Components/MyActivity/UserTalentList/UserTalentList';
 import UserCreatorList from '../../Components/MyActivity/UserCreatorList/UserCreatorList';
+import CardContributor from '../../Components/MyActivity/CardContributor/CardContributor';
 
-const MyActicity = () =>{
-    const classes = useStyles();
-    return(
-        <Box>
-            <PrimaryHeader/>
-            <UserActivityList/>
-            <br></br>
-            <UserContributorList/>
-            <br></br>
-            <UserTalentList/>
-            <br></br>
-            <UserCreatorList/>
-        </Box>
-    )
-}
+const MyActivity = () => {
+  const classes = useStyles();
+  return (
+    <Box>
+      <PrimaryHeader />
+      <Box className={classes.box}>
+        <UserActivityList />
+        <br></br>
+        <UserContributorList />
+        <br></br>
+        <UserTalentList />
+        <br></br>
+        <UserCreatorList />
+        <br></br>
+        <CardContributor/>
+      </Box>
+    </Box>
+  );
+};
 
-export default MyActicity;
+export default MyActivity;
--- client/web/src/Containers/MyActivity/styles.ts
+++ client/web/src/Containers/MyActivity/styles.ts
@@ -2,6 +2,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(
   theme => ({
-
+    box:{
+      padding: 10,
+    }
   }),
 );
