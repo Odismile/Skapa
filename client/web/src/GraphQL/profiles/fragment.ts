789b76e733ec2bb9c120f96f7ed0c002f3b88cd5
Herilova <herilova@hairun-technology.com>
2021-08-19 11:17:27 +0300

    fix gragment

--- client/web/src/GraphQL/profiles/fragment.ts
+++ client/web/src/GraphQL/profiles/fragment.ts
@@ -3,43 +3,50 @@ import gql from 'graphql-tag';
 export const COACH_INFO_FRAGMENT = gql`
   fragment CoachInfo on Profiles {
     id
+    created_at
     updated_at
     position
     bio
     picture
+    video
+    job_seniority_id {
+      id
+    }
+    users_id {
+      id
+    }
     published_at
     profile_skills {
       id
-      # created_at
-      # skill_id
-      # profile_id
-      # published_at
+      created_at
+      published_at
     }
     talent_favorits {
       id
-      # created_at
-      # coach_id
-      # talent_id
-      # published_at
+      created_at
+      published_at
     }
     project_favorits {
       id
       created_at
       project {
+        id
+        created_at
+        Name
+        description
       }
     }
     languages {
       id
       level
-      language
-      users_permissions_user
+      language {
+        id
+        label
+      }
+      published_at
     }
     projects {
       id
-      created_at
-      Name
-      description
     }
   }
 `;
-
