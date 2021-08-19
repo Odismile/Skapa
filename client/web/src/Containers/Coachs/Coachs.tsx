b8e7062936d2b946e3f113bb071151cc80009a2f
Herilova <herilova@hairun-technology.com>
2021-08-19 17:33:07 +0300

    search coach

--- client/web/src/Containers/Coachs/Coachs.tsx
+++ client/web/src/Containers/Coachs/Coachs.tsx
@@ -1,21 +1,11 @@
-import React, { useState } from "react";
-import {
-  Box,
-  Button,
-  IconButton,
-  Link,
-  Typography,
-  Modal,
-  Backdrop,
-  Fade
-} from '@material-ui/core';
-
-// date picker
-import { DatePicker } from "@material-ui/pickers";
-
-// component
+import React from 'react';
+import { Box, List, ListItem, TextField, Typography } from '@material-ui/core';
 import PrimaryHeader from '../../Components/Header/Header';
 import DailyRate from '../../Components/DailyRate/DailyRate';
+import Filter from '../../Components/Icons/Filter';
+import Search from '../../Components/Icons/Search';
+
+import useStyles from './styles';
 import CoachsItem from './CoachsItem/CoachsItem';
 import SearchFilterTalents from '../../Components/SearchFilterTalents/SearchFilterTalents';
 import TextFieldComponent from '../../Components/TextField/TextField';
@@ -40,6 +30,8 @@ import Booking from '../../Assets/images/booking.svg';
 
 const Coachs = () => {
   const classes = useStyles();
+
+  const filterTalent = useReactiveVar(filterTalentVar);
   const { data, loading } = useQuery<coachs, coachsVariables>(LIST_COACH, {
     variables: {
       where: {
@@ -49,7 +41,15 @@ const Coachs = () => {
       },
     },
   });
-  const listCoachs = (data?.profiles || []).filter((item) => item?.users_id);
+  const listCoachs = (data?.profiles || []).filter(
+    (item) =>
+      item &&
+      item?.users_id &&
+      (`${item?.users_id?.username}${item?.users_id?.surname}${item?.users_id?.lastname}${item?.users_id?.email}`
+        .toLowerCase()
+        .includes(filterTalent.search.toLowerCase()) ||
+        `${item?.position}`.toLowerCase().includes(filterTalent.search.toLowerCase())),
+  );
   return (
     <Box className={classes.mainPage}>
       <PrimaryHeader />
@@ -62,6 +62,17 @@ const Coachs = () => {
               Design Thinking Process
             </Typography> */}
             <Box className="item_list">
+              {loading &&
+                Array.from(Array(3).keys()).map(() => (
+                  <Skeleton
+                    width={'90%'}
+                    height="100px"
+                    style={{
+                      margin: 'auto',
+                    }}
+                  />
+                ))}
+
               {listCoachs.map((profil) => (
                 <CoachsItem
                   coachPhoto={profil?.picture || coachPhoto}
@@ -75,30 +86,6 @@ const Coachs = () => {
               ))}
             </Box>
           </Box>
-
-          {/* <Box className="item_bloc">
-              <Typography className="titre_item" component="h2">Design to Change Process</Typography>
-              <Box className="item_list">
-                <CoachsItem 
-                  coachPhoto={coachPhoto} 
-                  iconJob={DesignThinkerPicto}  
-                  jobTitle="Design Thinker"
-                  coachName="Louis Tomaso"
-                  coachAddress="Paris"
-                  coachLevel="Senior"
-                  coachFee= {500}
-                />
-                <CoachsItem 
-                  coachPhoto={coachPhoto} 
-                  iconJob={DesignThinkerPicto}  
-                  jobTitle="Design Thinker"
-                  coachName="Louis Tomaso"
-                  coachAddress="Paris"
-                  coachLevel="Senior"
-                  coachFee= {500}
-                />
-              </Box>
-            </Box> */}
 
           {/* <Box className="item_bloc">
             <Typography className="titre_item" component="h2">
