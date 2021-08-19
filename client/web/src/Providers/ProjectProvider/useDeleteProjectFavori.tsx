07060fbc876795c76227c4030fa1553eb89e8202
Keke <keke@hairun-technology.com>
2021-08-19 16:58:37 +0300

    delete favorite project mutation

---
+++ client/web/src/Providers/ProjectProvider/useDeleteProjectFavori.tsx
@@ -0,0 +1,30 @@
+import { useApolloClient, useMutation } from '@apollo/client';
+import { useTranslation } from 'react-i18next';
+import { DELETE_PROJECT_FAVORI } from '../../GraphQL/project/mutation';
+import { deleteProjectFavorit, deleteProjectFavoritVariables } from '../../GraphQL/project/types/deleteProjectFavorit';
+import { displaySnackbar, InitSnackbarData } from '../../Utils';
+
+export const useDeleteProjectFavori = () => {
+  const { t } = useTranslation();
+
+  const snackbar = InitSnackbarData;
+  const client = useApolloClient();
+
+  const [doDeleteProjectFavorit, { loading, data }] = useMutation<deleteProjectFavorit, deleteProjectFavoritVariables>(
+    DELETE_PROJECT_FAVORI,
+    {
+      onError: (error) => {
+        const errorMessage =
+          error?.graphQLErrors?.[0]?.extensions?.exception?.data?.message?.[0]?.messages?.[0]?.message;
+
+        if (errorMessage === undefined) {
+          snackbar.type = 'ERROR';
+          snackbar.message = t(`errorMessage.${errorMessage}`);
+          displaySnackbar(client, snackbar);
+        }
+        return;
+      },
+    },
+  );
+  return { doDeleteProjectFavorit, data, loading };
+};
