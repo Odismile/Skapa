2092d841bc2fc66c2d0bdeabfe65b1986dab6162
Fanasina Ny <fanasina@hairun-technology.com>
2021-08-19 14:03:30 +0300

    homepage Myactivity

--- client/web/src/Components/MyActivity/CardSpace/CardSpace.tsx
+++ client/web/src/Components/MyActivity/CardSpace/CardSpace.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { Box, Card, CardContent, Typography } from '@material-ui/core';
 import useStyles from './style';
-import imgActivity from '../../Assets/images/activity.png';
+import imgActivity from '../../../Assets/images/activity.png';
 
-const CardSpace = () => {
+interface CardSpaceProps{
+  title: string,
+  subltitle?: string,
+}
+const CardSpace:FC<CardSpaceProps> = (props) => {
   const classes = useStyles();
   return (
     <Card className={classes.card}>
       <CardContent className='contentCard'>
         <img src={imgActivity} alt='photo'/>
         <Box className='details'>
-          <Typography className='title'>Contributor space</Typography>
-          <Typography className='subtitle'>Lorem Ipsum</Typography>
+          <Typography className='title'>{props.title}</Typography>
+          <Typography className='subtitle'>{props.subltitle}</Typography>
         </Box>
       </CardContent>
     </Card>
