482e80ecb59cbdfe4afdf13976492db51841cfe4
Herilova <herilova@hairun-technology.com>
2021-08-20 16:07:24 +0300

    type

--- client/web/src/GraphQL/user/types/MeInfo.ts
+++ client/web/src/GraphQL/user/types/MeInfo.ts
@@ -11,6 +11,13 @@ import { PublicationState } from "./../../../types/graphql-global-types";
 
 export interface MeInfo_user {
   __typename: "UsersPermissionsUser";
+  id: string;
+  username: string;
+  surname: string | null;
+  lastname: string | null;
+  email: string;
+  confirmed: boolean | null;
+  blocked: boolean | null;
 }
 
 export interface MeInfo {
