7f0d402060ee63f7c99d5b5a1faaa690ebf8cb17
Herilova <herilova@hairun-technology.com>
2021-08-20 09:39:29 +0300

    remove inused import

--- client/web/src/Containers/Coachs/Coachs.tsx
+++ client/web/src/Containers/Coachs/Coachs.tsx
@@ -1,43 +1,24 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import {
-  Backdrop,
   Box,
-  Button,
-  IconButton,
-  List,
-  ListItem,
-  Modal,
-  TextField,
-  Typography,
-  Fade,
-  Link,
-  SwipeableDrawer,
 } from '@material-ui/core';
 import Skeleton from '@material-ui/lab/Skeleton';
 import PrimaryHeader from '../../Components/Header/Header';
-import DailyRate from '../../Components/DailyRate/DailyRate';
-import Filter from '../../Components/Icons/Filter';
-import Search from '../../Components/Icons/Search';
 
 import useStyles from './styles';
 import CoachsItem from './CoachsItem/CoachsItem';
 import SearchFilterTalents from '../../Components/SearchFilterTalents/SearchFilterTalents';
-import TextFieldComponent from '../../Components/TextField/TextField';
 
-// icons
-import Award from '../../Components/Icons/Award/Award';
-import Calendar from '../../Components/Icons/Calendar/Calendar';
-import Clock from '../../Components/Icons/Clock/Clock';
-import Cross from '../../Components/Icons/Cross/Cross';
+
 
 // images
 import coachPhoto from '../../Assets/images/coach_avatar.png';
 import DesignThinkerPicto from '../../Assets/images/thinker_picto.png';
-import Booking from '../../Assets/images/booking.svg';
 import { useQuery, useReactiveVar } from '@apollo/client';
 import { filterTalentVar } from '../../ReactiveVariable/Coach/coach';
 import { coachsVariables, coachs } from '../../GraphQL/profiles/types/coachs';
 import { LIST_COACH } from '../../GraphQL/profiles/query';
+import MeetingModal from './MeetingModal';
 
 const Coachs = () => {
   const classes = useStyles();
@@ -142,71 +123,7 @@ const Coachs = () => {
           </Box> */}
         </Box>
       </Box>
-      <SwipeableDrawer
-        className={classes.modalCalendar}
-        anchor="bottom"
-        open={openCalendar}
-        onClose={handleClose}
-        onOpen={handleOpen}
-        closeAfterTransition
-        classes={{
-          paper: classes.paperDrawer
-        }}
-        // BackdropComponent={Backdrop}
-        // BackdropProps={{
-        //   timeout: 500,
-        // }}
-      >
-        <Fade in={openCalendar}>
-          <Box className={classes.paperCalendar}>
-            <Button variant="contained" className="btn_handleDrawer" onClick={handleClose}></Button>
-            <Box className="content_calendar" style={{ display: 'block' }}>
-              <Box className="header_calendar" component="header">
-                <h2 id="transition-modal-title">Date and Time</h2>
-                <Button color="primary" variant="contained" className="btn_book">
-                  Book
-                </Button>
-              </Box>
-              <Box className="body_calendar" component="section">
-                <Box className="field_item">
-                  <TextFieldComponent
-                    label="Your selection"
-                    id="date-meeting"
-                    placeholder="dd/mm/yyyy"
-                    type="text"
-                    //value= {moment(startDate).format('h:mm')}
-                    value=""
-                    icons={<Calendar />}
-                  />
-                </Box>
-                <Box className="dateInline_bloc">
-                  {/* <DatePicker
-                  autoOk
-                  variant="static"
-                  openTo="year"
-                  value={date}
-                  onChange={changeDate}
-                /> */}
-                </Box>
-              </Box>
-            </Box>
-
-            <Box className="valid_calendar">
-              <h2>Thank you !</h2>
-              <Box className="img_path">
-                <img src={Booking} alt="Booking" />
-              </Box>
-              <Box className="text_bloc">
-                <Typography>
-                  You’ve booked an appointment with <span className="purple_text">Louis Tomaso</span> for the{' '}
-                  <span className="purple_text">12/05/21</span> at <span className="purple_text">10 A.M</span>
-                </Typography>
-                <Link className="link">Add the meeting to your Google Calendar</Link>
-              </Box>
-            </Box>
-          </Box>
-        </Fade>
-      </SwipeableDrawer>
+      <MeetingModal open={openCalendar} handleClose={handleClose} handleOpen={handleOpen} />
     </Box>
   );
 };
