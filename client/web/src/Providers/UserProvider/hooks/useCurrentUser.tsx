8fda91e804a6c69adead02f4a0b477d5611513b9
Keke <keke@hairun-technology.com>
2021-08-23 17:37:57 +0300

    fix user to get profil

--- client/web/src/Providers/UserProvider/hooks/useCurrentUser.tsx
+++ client/web/src/Providers/UserProvider/hooks/useCurrentUser.tsx
@@ -3,16 +3,20 @@ import { ME_INFO } from '../../../GraphQL/user/query';
 import { MeInfo, MeInfoVariables } from '../../../GraphQL/user/types/MeInfo';
 
 export const useCurrentUser = () => {
+  let userId = 0;
+  if (localStorage.getItem('idMe')) {
+    userId = Number(localStorage.getItem('idMe'));
+  }
   const result = useQuery<MeInfo, MeInfoVariables>(ME_INFO, {
     variables: {
-      id: `${localStorage.getItem('idMe')}`,
+      userId: userId,
     },
   });
   return {
-    profilId: result.data?.profile?.id ?? null,
-    user: result.data?.profile?.users_id ?? null,
-    photo: result.data?.profile?.picture ?? null,
-    isReader: result.data?.profile?.users_id?.isFirstConnection === true ? true : false,
+    profilId: result.data?.getProfile?.id ?? null,
+    user: result.data?.getProfile?.users_id ?? null,
+    photo: result.data?.getProfile?.picture ?? null,
+    isReader: result.data?.getProfile?.users_id?.isFirstConnection === true ? true : false,
     //isReader: false,
   };
 };
