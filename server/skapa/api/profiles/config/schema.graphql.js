c2a085c27857b55b85fbc64e0f3b066a440847b0
mimiee <mendrikaran@hairun-technology.com>
2021-08-23 17:06:55 +0200

    server: fix-profiles-mutation

---
+++ server/skapa/api/profiles/config/schema.graphql.js
@@ -0,0 +1,52 @@
+module.exports = {
+  type: {
+    UsersPermissionsPermission: false,
+  },
+  definition: /* GraphQL */ `
+    enum Level {
+      BASIC
+      INTERMEDIATE
+      FLUENT
+    }
+
+    type projects {
+      id: ID!
+      item: String
+    }
+
+    type languages {
+      id: ID!
+      level: Level
+    }
+
+    type skills {
+      id: ID!
+      skills_id: String
+    }
+
+    type ProfileCustomizeMe {
+      id: ID!
+      bio: String
+      picture: String
+      video: String
+      job_seniority: String
+      project: projects
+      language: languages
+      skill: skills
+    }
+
+    type profileCustomizePayload {
+      profile: ProfileCustomizeMe
+    }
+
+    input profileCustomizeMeInput {
+      bio: String
+      picture: String
+      video: String
+      job_seniority: String
+    }
+  `,
+  mutation: `
+    profileCustomized(input: profileCustomizeMeInput!): profileCustomizePayload!
+`,
+};
