cb117aa4c6e069469e4b326a146918e0ab8dfe44
hasina <hasina@hairun-technology.com>
2021-08-23 21:57:32 +0300

    Fix delete talent favorits

--- server/skapa/api/talent-favorits/config/schema.graphql.js
+++ server/skapa/api/talent-favorits/config/schema.graphql.js
@@ -1,20 +1,19 @@
 module.exports = {
-    definition: ``,
-    query: ``,
-    mutation: `
-      deleteTalentFavorits(profileId:Int): Int
+  definition: ``,
+  query: ``,
+  mutation: `
+      deleteTalentFavorits(userId:Int, talentId:Int):Int
     `,
-    type: {},
-    resolver: {
-      Query: {},
-      Mutation: {
-        deleteTalentFavorits: {
-          description: "Delete talent-favorits by profile-id",
-          resolverOf: "application::talent-favorits.talent-favorits.delete",
-          resolver:
-            "application::talent-favorits.talent-favorits.deleteTalentFavorits",
-        },
+  type: {},
+  resolver: {
+    Query: {},
+    Mutation: {
+      deleteTalentFavorits: {
+        description: "Delete talent-favorits by coachId and talentId",
+        resolverOf: "application::talent-favorits.talent-favorits.delete",
+        resolver:
+          "application::talent-favorits.talent-favorits.deleteTalentFavorits",
       },
     },
-  };
-  
+  },
+};
--- server/skapa/api/talent-favorits/controllers/talent-favorits.js
+++ server/skapa/api/talent-favorits/controllers/talent-favorits.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
@@ -6,18 +6,20 @@
  */
 
 module.exports = {
-    async deleteTalentFavorits(ctx) {
-        //const { profileId } = ctx.request.body;
-        console.log(ctx.request.body);
-        /*const results = await strapi
-          .query("talent-favorits")
-          .find({ profile: profileId });
-    
-        let entity;
-        if (results && results[0]) {
-          entity = results[0];
-          strapi.query("talent-favorits").delete({ id: entity.id });
-        }
-        return entity ? entity.id : 0;*/
-      },
+  async deleteTalentFavorits(ctx) {
+    const { userId, talentId } = ctx.request.body;
+    const profile = await strapi
+      .query("profiles")
+      .findOne({ users_id: userId });
+    const results = await strapi
+      .query("talent-favorits")
+      .find({ coach_id: profile.id, talent_id: talentId });
+
+    let entity;
+    if (results && results[0]) {
+      entity = results[0];
+      strapi.query("talent-favorits").delete({ id: entity.id });
+    }
+    return entity ? entity.id : 0;
+  },
 };
