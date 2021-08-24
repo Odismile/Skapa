5339a1b65292fbbd838713e66aafc1895a379c5b
Herilova <herilova@hairun-technology.com>
2021-08-24 14:22:35 +0300

    update hooks

--- client/web/src/Providers/UserProvider/hooks/useCurrentUser.tsx
+++ client/web/src/Providers/UserProvider/hooks/useCurrentUser.tsx
@@ -12,9 +12,12 @@ export const useCurrentUser = () => {
     fetchPolicy: 'cache-first',
   });
   return {
+    loading: result.loading,
+    error: result.error,
     profilId: result.data?.getProfile?.id ?? null,
     user: result.data?.getProfile?.users_id ?? null,
     photo: result.data?.getProfile?.picture ?? null,
+    profil : result?.data?.getProfile ?? null,
     isReader: result.data?.getProfile?.users_id?.isFirstConnection === true ? true : false,
     //isReader: false,
   };
