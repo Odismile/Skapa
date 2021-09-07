210e7de0f39f4c4257df460987bcaeccc308506b
Johnny Rakoto <johnny.rakotoarison@gmail.com>
2021-09-07 17:59:24 +0300

    Request Item update

--- client/web/src/Containers/AppExterne/RequestItem/RequestItem.tsx
+++ client/web/src/Containers/AppExterne/RequestItem/RequestItem.tsx
@@ -5,11 +5,12 @@ import classNames from 'classnames';
 import useStyles from './style';
 
 import Calendar from '../../../Components/Icons/Calendar';
-import Checked from '../../../Components/Icons/CheckboxChecked';
+import Check from '../../../Components/Icons/Check';
 import Cross from '../../../Components/Icons/Cross';
 
 import Team from '../../../Components/Team/Team';
 import UserAvatar from '../../Assets/images/user_avatar.jpg';
+import requestPhoto from '../../../Assets/images/photo_request.png';
 
 
 const RequestItem = () => {
@@ -17,42 +18,44 @@ const RequestItem = () => {
   
   return (
     <Card className={classNames(classes.requestItem_bloc, "requestItem_bloc")}>
-      <CardActionArea>
-        <CardMedia
-          className="media"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Box className="header_item" component="header">
-            <Typography variant="h2">Lorem Ipsum Sit Amet</Typography>
-            <Typography className="date_range"><Calendar /><span>20/03/21 - 20/06/21</span></Typography>
+      
+      <CardContent className={classes.cardContentroot} >
+        <Box className="cardMedia_content">
+          <CardMedia
+            className="media"
+            image={requestPhoto}
+            title="request show"
+          />
+          <Typography component="span" className="labeled">Innovation</Typography>
+        </Box>
+        <Box className="header_item" component="header">
+          <Typography variant="h2">Lorem Ipsum Sit Amet</Typography>
+          <Typography className="date_range"><Calendar /><span>20/03/21 - 20/06/21</span></Typography>
+        </Box>
+        <Box className="body_item" component="article">
+          <Box className="infos_content">
+            <Typography className=""><label>Founder :</label><span><strong>Johanna Lorin</strong> - Lorem - Paris</span></Typography>
+            <Team />
           </Box>
-          <Box className="body_item" component="article">
-            <Box className="infos">
-              <Typography className=""><label>Founder :</label><span><strong>Johanna Lorin</strong> - Lorem - Paris</span></Typography>
-              <Team />
-            </Box>
-            <Box className="skills_recquired">
-              <label>Skills recquired :</label>
-              <Box className="skills_list">
-                <Typography className="skill_item" component="span">HTML/CSS</Typography>
-                <Typography className="skill_item" component="span">Angular</Typography>
-                <Typography className="skill_item" component="span">Protopie</Typography>
-              </Box>
+          <Box className="skills_recquired">
+            <label>Skills recquired :</label>
+            <Box className="skills_list">
+              <Typography className="skill_item" component="span">HTML/CSS</Typography>
+              <Typography className="skill_item" component="span">Angular</Typography>
+              <Typography className="skill_item" component="span">Protopie</Typography>
             </Box>
           </Box>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <IconButton aria-label="validate">
-          <Checked />
+        </Box>
+      </CardContent>
+      
+      <CardActions className="btns-group" >
+        <IconButton aria-label="validate" className="btn btn_validate">
+          <Check />
         </IconButton>
-        <IconButton aria-label="remove">
+        <IconButton aria-label="remove" className="btn btn_remove">
           <Cross />
         </IconButton>
       </CardActions>
-      
     </Card>
   );
 };
