c85329755d45cc89053066008ccb8230fb5d116a
Johnny Rakoto <johnny.rakotoarison@gmail.com>
2021-09-08 18:02:32 +0300

    team

--- client/web/src/Components/Team/Team.tsx
+++ client/web/src/Components/Team/Team.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Avatar } from '@material-ui/core';
+import { Avatar, Box, Typography} from '@material-ui/core';
 import AvatarGroup from '@material-ui/lab/AvatarGroup';
 import classNames from 'classnames';
 import useStyles from './style';
@@ -10,13 +10,16 @@ const Team = () => {
   const classes = useStyles();
   
   return (
-    <AvatarGroup max={3} className={classes.avatarList}>
-      <Avatar className={classes.avatarItem} alt="User 1" src={UserAvatar} />
-      <Avatar alt="User 2" src={UserAvatar} />
-      <Avatar alt="User 3" src={UserAvatar} />
-      <Avatar alt="User 4" src={UserAvatar} />
-      <Avatar alt="User 5" src={UserAvatar} />
-    </AvatarGroup>
+    <Box className={classes.team_bloc}>
+      <Typography className="team_title">Team</Typography>
+      <AvatarGroup max={3} className={classes.avatarList}>
+        <Avatar className={classes.avatarItem} alt="User 1" src={UserAvatar} />
+        <Avatar alt="User 2" src={UserAvatar} />
+        <Avatar alt="User 3" src={UserAvatar} />
+        <Avatar alt="User 4" src={UserAvatar} />
+        <Avatar alt="User 5" src={UserAvatar} />
+      </AvatarGroup>
+    </Box>
   );
 };
 
--- client/web/src/Components/Team/style.ts
+++ client/web/src/Components/Team/style.ts
@@ -2,6 +2,17 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(
   theme => ({
+    team_bloc: {
+      display: 'flex',
+      alignItems: 'center',
+      "& .team_title": {
+        fontSize: 10,
+        display: 'inline-flex',
+        fontWeight: 700,
+        lineHeight: 1.25,
+        marginRight: 5,
+      },
+    },
     avatarList: {
 
     },
