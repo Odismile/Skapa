75a354718043408079d71d2fc0a0db3de2186117
Herilova <herilova@hairun-technology.com>
2021-09-14 18:48:32 +0300

    custom hooks

---
+++ client/web/src/Providers/ProjectProvider/useCreateTeamProject.tsx
@@ -0,0 +1,44 @@
+import { useApolloClient, useMutation } from "@apollo/client";
+import { useTranslation } from "react-i18next";
+import { ADD_TALENT_TO_PROJECT } from "../../GraphQL/project/mutation";
+import { PROJECTS } from "../../GraphQL/project/query";
+import { CreateTeam, CreateTeamVariables } from "../../GraphQL/project/types/CreateTeam";
+import { displaySnackbar, InitSnackbarData } from "../../Utils";
+
+
+export const useCreateContribution = () => {
+  const { t } = useTranslation();
+
+  const snackbar = InitSnackbarData;
+  const client = useApolloClient();
+
+  const resp = useMutation<CreateTeam, CreateTeamVariables>(
+    ADD_TALENT_TO_PROJECT,
+    {
+      onError: (error) => {
+        const errorMessage =
+          error?.graphQLErrors?.[0]?.extensions?.exception?.data?.message?.[0]?.messages?.[0]?.message;
+
+        if (errorMessage === undefined) {
+          snackbar.type = 'ERROR';
+          snackbar.message = t(`errorMessage.${errorMessage}`);
+          displaySnackbar(client, snackbar);
+        }
+        return;
+      },
+      onCompleted: (data) => {
+        if (data.createTeam?.team?.id) {
+          snackbar.type = 'SUCCESS';
+          snackbar.message = t(`createFavorit.success`);
+          displaySnackbar(client, snackbar);
+        }
+      },
+      refetchQueries: [
+        {
+          query: PROJECTS,
+        },
+      ],
+    },
+  );
+  return resp;
+};
