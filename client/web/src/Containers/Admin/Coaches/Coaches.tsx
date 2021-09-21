430b8e6db69293d7870bb7d555628ff50848837c
Johnny Rakoto <johnny.rakotoarison@gmail.com>
2021-09-21 17:36:28 +0300

    Coaches

---
+++ client/web/src/Containers/Admin/Coaches/Coaches.tsx
@@ -0,0 +1,153 @@
+import React from 'react';
+import { 
+  Box, 
+  Button, 
+  IconButton, 
+  Typography,
+  Badge,
+  Divider,
+} from '@material-ui/core';
+
+import SearchFilter from '../../../Components/SearchFilter/SearchFilter';
+import CardProjectsPending from '../CardProjectsPending';
+
+import DrawerMenuAdmin from '../DrawerMenuAdmin';
+import UserProfilAdmin from '../UserProfilAdmin';
+
+// icons
+import MenuIcon from '@material-ui/icons/Menu';
+import Prohibition from '../../../Components/Icons/Prohibition';
+import Plus from '../../../Components/Icons/Plus';
+import Trash from '../../../Components/Icons/Trash';
+
+import useStyles from './styles';
+
+
+// image
+import UserAvatar from '../../../Assets/images/user_avatar.jpg'; 
+
+const columns = [
+  {
+    field: 'Name',
+    flex: 1,
+    editable: false,
+    sortable: false,
+    headerName: 'Name',
+    minWidth: 120,
+    disableColumnMenu: true,
+  },
+  {
+    field: 'Type',
+    headerName: 'Type',
+    width: 120,
+    
+    editable: false,
+    sortable: false,
+    disableColumnMenu: true,
+    disableExtendRowFullWidth: true,
+  },
+  {
+    field: 'Creator',
+    flex: .5,
+    headerName: 'Creator',
+    minWidth: 150,
+    editable: false,
+    sortable: false,
+    disableColumnMenu: true,
+  },
+];
+const rows = [
+  { id: 1, Name: '1 Lorem Ipsum Sit Amet', Type: 'Innovation 1', Creator: 'E. Dupont' },
+  { id: 2, Name: '2 Lorem Ipsum Sit Amet', Type: 'Innovation 2', Creator: 'E. Dupont' },
+  { id: 3, Name: '3 Lorem Ipsum Sit Amet', Type: 'Innovation 3', Creator: 'E. Dupont' },
+  { id: 4, Name: '4 Lorem Ipsum Sit Amet', Type: 'Innovation 4', Creator: 'E. Dupont' },
+  { id: 5, Name: '5 Lorem Ipsum Sit Amet', Type: 'Innovation 5', Creator: 'E. Dupont' },
+  { id: 6, Name: '6 Lorem Ipsum Sit Amet', Type: 'Innovation 6', Creator: 'E. Dupont' },
+  { id: 7, Name: '7 Lorem Ipsum Sit Amet', Type: 'Innovation 7', Creator: 'E. Dupont' },
+  { id: 8, Name: '8 Lorem Ipsum Sit Amet', Type: 'Innovation 8', Creator: 'E. Dupont' },
+  { id: 9, Name: '9 Lorem Ipsum Sit Amet', Type: 'Innovation 9', Creator: 'E. Dupont' },
+  { id: 10, Name: '10 Lorem Ipsum Sit Amet', Type: 'Innovation 10', Creator: 'E. Dupont' },
+  { id: 11, Name: '11 Lorem Ipsum Sit Amet', Type: 'Innovation 11', Creator: 'E. Dupont' },
+  { id: 12, Name: '12 Lorem Ipsum Sit Amet', Type: 'Innovation 12', Creator: 'E. Dupont' },
+  { id: 13, Name: '13 Lorem Ipsum Sit Amet', Type: 'Innovation 13', Creator: 'E. Dupont' },
+  { id: 14, Name: '14 Lorem Ipsum Sit Amet', Type: 'Innovation 14', Creator: 'E. Dupont' },
+  { id: 15, Name: '15 Lorem Ipsum Sit Amet', Type: 'Innovation 15', Creator: 'E. Dupont' },
+  { id: 16, Name: '16 Lorem Ipsum Sit Amet', Type: 'Innovation 16', Creator: 'E. Dupont' },
+  { id: 17, Name: '17 Lorem Ipsum Sit Amet', Type: 'Innovation 17', Creator: 'E. Dupont' },
+];
+
+const ProjectsAdmin = () => {
+  const classes = useStyles();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+  return (
+    <Box className={classes.CoachesAdminPage}>
+      {/* drawer Menu admin */}
+      <Box className="asideLeftWrapper">
+        <DrawerMenuAdmin open={mobileOpen} onClose={handleDrawerToggle} />
+      </Box>
+
+      <main className={classes.content}>
+        <Box className="wrap_content">
+          <IconButton
+            color= "inherit"
+            aria-label= "open drawer"
+            edge= "start"
+            onClick={handleDrawerToggle}
+            className="menuButton"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Box className="header_admin" component="header">
+            <Box className="top_header">
+              {/* page title */}
+              <Typography className="title_page" variant="h2">Coaches</Typography>
+              
+              {/* user profile connected */}
+              <UserProfilAdmin photoAvatar={UserAvatar} userName="Pierre-Marie" userJob="Administrateur" />
+            </Box>
+            {/* filter bloc */}
+            <Box className="filter_list">
+              <SearchFilter placeholder="Look for a project" />
+              <IconButton className="btn warning_btn" aria-label="warning">
+                <Prohibition />
+              </IconButton>
+              <IconButton className="btn add_btn" aria-label="add">
+                <Plus />
+              </IconButton>
+              <IconButton className="btn remove_btn" aria-label="remove">
+                <Trash />
+              </IconButton>
+
+            </Box>
+          </Box>
+          <Box className="mainContent_admin" component="article">
+            {/* list of projects */}
+            <Box className="content_bloc">
+              <Box className="coaches_list">
+                <Box className="coaches_group">
+                  {/* card item */}
+                  
+                </Box>
+              </Box>
+              <Divider orientation="vertical" flexItem />
+              <Box className="editCoaches_content">
+                <Typography className="title_block" variant="h3">Add a new coach</Typography>
+                <Box className="coaches_forms">
+                
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </main>
+    </Box>
+  );
+};
+
+
+
+
+export default ProjectsAdmin;
---
+++ client/web/src/Containers/Admin/Coaches/styles.ts
@@ -0,0 +1,293 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(
+  (theme) => ({
+    ProjectAdminPage: {
+      display: 'flex',
+      "& .asideLeftWrapper": {
+
+      },
+      "& .menuButton": {
+        marginRight: theme.spacing(2),
+        [theme.breakpoints.up('sm')]: {
+          display: 'none',
+        },
+      },
+    },
+    drawer: {
+      display: 'flex',
+      [theme.breakpoints.up('sm')]: {
+        width: 185,
+        flexShrink: 0,
+      },
+      "& .sideBarLeft": {
+        background: '#8870ff',
+        borderRadius: "0 40px 40px 0",
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+        padding: "30px 30px 15px",
+        "& .logo": {
+          margin: '0 0 80px',
+          textAlign: 'center',
+          "& img": {
+            height: 53,
+            maxWidth: '100%',
+          },
+        },
+        
+        "& .topInnovator": {
+          margin: 0,
+          textAlign: 'center',
+          "& img": {
+            height: 'auto',
+            maxWidth: '100%',
+          },
+        },
+      },
+    },
+    flexFx: {
+      flex: '1 0 0%',
+    },
+    drawerPaper: {
+      border: 'none',
+      width: 185,
+    },
+    content: {
+      flexGrow: 1,
+      padding: '1.25rem 1rem',
+      "@media (min-width: 960px)": {
+        padding: '1.25rem 2rem 2rem 3rem',
+      },
+      "& .header_admin": {
+        "& .top_header": {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          margin: '0 0 20px',
+        },
+        "& .title_page": {
+          color: '#000',
+          fontSize: 28,
+          fontWeight: 700,
+          letterSpacing: .56,
+          lineHeight: 1.25,
+          margin: 0,
+        },
+        "& .filter_list": {
+          display: 'flex',
+          alignItems: 'center',
+          maxWidth: "50%",
+          marginBottom: 30,
+          "& .input-search": {
+            width: 305,
+          },
+          "& .btn": {
+            border: '1px solid #8870ff',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginLeft: 8,
+            "& svg": {
+              fill: "#8870ff",
+              fontSize: "1rem",
+            },
+            "&:hover": {
+              background: "rgba(242,241,253,1)",
+            },
+          },
+        },
+      },
+      "& .mainContent_admin": {
+        
+      },
+      "& .allProjects_list": {
+        "@media (min-width: 960px)": {
+          flexBasis: '55%',
+          width: '55%',
+          paddingRight: '2rem',
+        },
+       
+      },
+      "& .title_block": {
+        color: '#8870ff',
+        fontSize: 14,
+        fontWeight: 700,
+        marginBottom: 20,
+        "& .nbr": {
+          background: '#ece8ff',
+          borderRadius: 20,
+          fontSize: 10,
+          fontWeight: 700,
+          marginLeft: 10,
+          padding: 0,
+          "& span": {
+            position: 'static',
+            transform: 'none',
+            padding: 0,
+          },
+        },
+      },
+      "& .content_bloc": {
+        "& .tri_btns": {
+          display: 'flex',
+          alignItems: 'center',
+          marginBottom: 20,
+          width: "100%",
+          "& h3": {
+            color: "#383838",
+            fontSize: 14,
+            fontWeight: 400,
+            marginBottom: 0,
+          },
+          "& .btn": {
+            borderRadius: 16,
+            border: '1px solid #8870ff',
+            color: "#8870ff",
+            fontSize: 12,
+            fontWeight: 400,
+            textAlign: 'center',
+            padding: '0 10px',
+            height: 32,
+            margin: '0 5px 0 0',
+            "&:hover": {
+              backgroundColor: '#ece8ff',
+            },
+            "&:last-child": {
+              marginRight: 0,
+            },
+          },
+        },
+        overflow: 'hidden',
+        "@media (min-width: 960px)": {
+          display: 'flex',
+          justifyContent: 'space-between',
+          height: 'calc(100vh - 200px)',
+        },
+      },
+      "& .pendingProjects_content": {
+        "@media (min-width: 960px)": {
+          flexBasis: '45%',
+          padding: '0 0 0 30px',
+          width: '45%',
+        },
+      },
+      "& .pendingProjects_list": {
+        "@media (min-width: 960px)": {
+          height: 'calc(100% - 40px)',
+          overflow: 'hidden auto',
+          paddingRight: '1rem',
+        },
+      },
+    },
+    dataTable_wrapper: {
+      height: "calc(100vh - 245px)",
+      width: "100%",
+      "& .MuiDataGrid-root": {
+        border: "none",
+        marginBottom: 0,
+      },
+      '& .MuiDataGrid-renderingZone': {
+        maxHeight: 'calc(100vh - 380px) !important',
+        maxWidth: 'calc(100% - 20px)',
+        overflow: 'hidden auto',
+        transform: 'none !important',
+        //maxHeight: 'none !important',
+        "&::-webkit-scrollbar": {
+          width: 5,
+          borderRadius: 5,
+        },
+        "&::-webkit-scrollbar-track": {
+          //boxShadow: `inset 0 0 6px rgba(0, 0, 0, 0.3)`,
+        },
+        "&::-webkit-scrollbar-thumb": {
+          backgroundColor: "#dfdfdf",
+        },
+      },
+      '& .MuiDataGrid-row': {
+          //maxHeight: 'none !important',
+      },
+      "& .MuiDataGrid-main": {
+        //backgroundColor: "darkcyan",
+      },
+      "& .MuiDataGrid-columnsContainer": {
+        border: "none",
+      },
+      "& .MuiDataGrid-columnHeaderWrapper": {
+        padding: '0 20px',
+        minWidth: '100% !important',
+        width: 'calc(100% - 40px)',
+        "& .MuiDataGrid-columnHeaderCheckbox": {
+
+        },
+      },
+      "& .MuiDataGrid-columnSeparator": {
+        display: 'none',
+      },
+      "& .MuiDataGrid-columnHeaderTitleContainer": {
+        // padding: "0 1rem",
+      },
+      "& .MuiDataGrid-columnHeaderTitle": {
+        color: '#000',
+        fontSize: 14,
+        fontWeight: 600,
+        "&:focus": {
+          outline: 'none',
+        },
+      },
+      "& .MuiDataGrid-columnHeader:focus-within, & .MuiDataGrid-cell:focus-within": {
+        outline: 'none',
+      },
+      "& .MuiDataGrid-checkboxInput": {
+        color: '#8870ff',
+      },
+      "& .MuiDataGrid-window": {
+        background: '#fff',
+        borderRadius: 12,
+        border:  '1px solid #f4f4f4',
+        boxShadow: '0px 2px 4px rgba(0,0,0,0.11)',
+        padding: '20px',
+        overflowX: 'hidden',
+        overflowY: 'hidden !important',
+        "@media (min-width: 1024px)": {
+        },
+      },
+
+      "& .MuiDataGrid-dataContainer": {
+        "& .MuiDataGrid-row": {
+          maxWidth: "calc(100% - 40px)",
+          overflow: 'hidden',
+          "&:hover": {
+            background: "rgb(242,241,253)",
+          },
+        },
+      },
+      "& .MuiDataGrid-viewport": {
+        minWidth: '100% !important',
+        maxWidth: '100% !important',
+      },
+      "& .MuiDataGrid-cell": {
+        borderColor: "#e8e8e8",
+        color: '#383838',
+        fontSize: 12,
+        fontWeight: 400,
+        //lineHeight: 'unset !important',
+        //maxHeight: 'none !important',
+        whiteSpace: 'normal',
+        "&:focus": {
+          outline: 'none',
+        },
+      },
+      "& .MuiDataGrid-footerContainer": {
+        //display: 'none',
+        "& p": {
+          margin: 0,
+        },
+        "& button": {
+          padding: 0,
+        },
+      },
+    },
+  }),
+  { name: 'Admin' },
+);
