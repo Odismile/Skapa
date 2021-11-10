3b9ee93cc1613818349e2a241162d6229afb093f
mimiee <ralisonmendrikasarah@gmail.com>
2021-11-10 15:07:38 +0300

    add-create-profil-auth-google

--- client/web/src/Containers/Authentication/Google/Google.tsx
+++ client/web/src/Containers/Authentication/Google/Google.tsx
@@ -1,16 +1,24 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
-import { idMe, login } from '../../../ReactiveVariable/User/user';
+import { idMe } from '../../../ReactiveVariable/User/user';
 import { setAccessToken } from '../../../Services';
 
 import { useHistory } from 'react-router';
 import { HOMEPAGE, ONBOARDING_PROFILE7 } from '../../../Routes';
-import { AUTH_GOOGLE, CALLBACK_GOOGLE } from '../../../Constants/Config';
+import { CALLBACK_GOOGLE } from '../../../Constants/Config';
+import { useCreateProfile } from '../../../Providers/ProfilProvider/useCreateProfile';
+import { useTranslation } from 'react-i18next';
+import { Level } from '../../../types/graphql-global-types';
 
 const Google = () => {
   const location = useLocation();
+  const { t } = useTranslation();
   const history = useHistory();
+  const { doCreateProfile } = useCreateProfile();
+  const jobSeniorityDefault = '6';
+  const bioDefault = 'Tell you about me';
+  const languagesDefault = [{id: '1', level: Level.FLUENT}];
   useEffect(() => {
     if (!location) {
       return;
@@ -27,6 +35,18 @@ const Google = () => {
         setAccessToken(res.data.jwt);
         idMe(res.data.user.id);
         localStorage.setItem('idMe', res.data.user.id);
+        if(window.confirm(t('createProfile.isCreate'))) {
+          doCreateProfile({
+            variables: {
+              input: {
+                job_seniority: jobSeniorityDefault,//>8yrs
+                languages: languagesDefault,
+                bio: bioDefault,
+                user_id: res.data.user.id,
+              },
+            },
+          });
+        }
         history.replace(ONBOARDING_PROFILE7);
         window.location.reload();
       } else {
